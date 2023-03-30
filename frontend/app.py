ec5339b30ac9264190f0e214121db9d9fbc9d9e5
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-03-30 10:33:44 +0200

    adding files for frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("template/index.html")
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("plot.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
